5d3315f69ae043d8ab5d2ccd1112171426c57041
Deepak Mishra <deepak29mishra@gmail.com>
2020-01-05 22:11:09 +0530

    Add project files.

---
+++ testandoid/Control/Common/navbar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace testandoid.Control.Common
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class navbar : ContentPage
+    {
+        public navbar()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("hghghg", "gfgfg", "Cancel");
+        }
+    }
+}
